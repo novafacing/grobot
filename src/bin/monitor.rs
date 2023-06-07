eb2400db93ed210eaf75b4ea3d332f929ddaa7fe
novafacing <rowanbhart@gmail.com>
2023-06-06 22:44:06 -0700

    Add log fmt line

--- src/bin/monitor.rs
+++ src/bin/monitor.rs
@@ -1,10 +1,11 @@
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use clap::Parser;
 use grobot::{NetworkUpdate, PORT};
 use serde_json::from_str;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use tokio::net::UdpSocket;
-use tracing::{info, Level};
+use tracing::{info, subscriber::set_global_default, Level};
+use tracing_subscriber::FmtSubscriber;
 
 const BIND_ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
 
@@ -24,15 +25,24 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
+
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(args.log_level)
+        .finish();
+
+    set_global_default(subscriber)?;
+
     let bind_addr = SocketAddrV4::new(args.listen_addr, args.port);
     let sock = UdpSocket::bind(bind_addr).await?;
     sock.set_broadcast(true)?;
+    ensure!(sock.broadcast()?, "Can't receive from broadcast address");
 
     loop {
         // Receive on the socket
+        info!("Waiting for message");
         let mut buf = vec![0u8; 1024];
-        let (len, addr) = sock.recv_from(&mut buf).await?;
-        info!("Received {} bytes from {}", len, addr);
+        let len = sock.recv(&mut buf).await?;
+        info!("Received {} bytes", len);
         let update: NetworkUpdate = from_str(&String::from_utf8_lossy(&buf[..len]))?;
         info!("Received update {:?}", update);
     }
